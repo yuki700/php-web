763a91e0ac8b69bfaefddc1081c74799e3dd184a
yuki700 <yuki.takatori@g.pascalia.co.jp>
2021-06-21 17:53:43 +0900

    using php to render html content

--- modules/partials/header.php
+++ modules/partials/header.php
@@ -1,47 +1,56 @@
+<?php
+// Define an array to contain page titles
+$pageTitles = array(
+    'home' => "Home",
+    "profile" => "My Profile",
+);
+// Get page title depend on what is using module. 
+$pageTitle = $pageTitles[$module];
+?>
 <!DOCTYPE html>
 <html lang="en">
-  <head><link rel="stylesheet" href="normalize.css">
-	<meta charset="utf-8">
-	<title>My web page</title>
-	<link rel="stylesheet" href="./assets/css/index.css" />
-	<link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
-	
-	<script src="./assets/js/index.js"></script>
-  </head>
-  <body>
-	  <!-- The Header -->
-	  <header> 
-		<div>
-			<h4>The logo</h4>
-		</div>
-		<div>
-			  <h2 class="slogan">The header slogan</h2>
-		</div>
-		<div id="form">
-			<ul>
-				<li>Hi <span>Guest</span></li>
-				<li><a href="javascript:void(0)" onclick="showLoginForm()">Login</a></li>
-			</ul>
-			
-			<form id="login">
-				<input type="text" name="username" placeholder="User name" />
-				<input type="password" name="password" placeholder="Password"/>
-				<label><input type="checkbox" name="rememberUsername" />Remember user name </label>
-				<button type="submit" name="Login">Login</button>
-			</form>
-			<form method="GET" id="search">
-				<input type="text" name="keyword" />
-				<i class="material-icons">search</i>
-			</form>
-		</div>
-	  </header>
+    <head>
+        <link rel="stylesheet" href="normalize.css">
+        <meta charset="utf-8">
+        <!-- use defined variable to render page title in HTML -->
+        <title><?php echo $pageTitle; ?></title>
+        <link rel="stylesheet" href="./assets/css/index.css" />
+        <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
 
-      <!-- The menu -->
-	  <nav>
-		  <ul>
-			  <li>Menu 1</li>
-			  <li>Menu 2</li>
-			  <li>Menu 3</li>
-			  <li>Menu 4</li>
-		  </ul>
-	  </nav>
+        <script src="./assets/js/index.js"></script>s
+    </head>
+    <body>
+        <!-- The Header -->
+        <header> 
+        <div>
+            <h4>The logo</h4>
+        </div>
+        <div>
+                <h2 class="slogan">The header slogan</h2>
+        </div>
+        <div id="form">
+            <ul>
+                <li>Hi <span>Guest</span></li>
+                <li><a href="javascript:void(0)" onclick="showLoginForm()">Login</a></li>
+            </ul>
+            
+            <form id="login">
+                <input type="text" name="username" placeholder="User name" />
+                <input type="password" name="password" placeholder="Password"/>
+                <label><input type="checkbox" name="rememberUsername" />Remember user name </label>
+                <button type="submit" name="Login">Login</button>
+            </form>
+            <form method="GET" id="search">
+                <input type="text" name="keyword" />
+                <i class="material-icons">search</i>
+            </form>
+        </div>
+        </header>
+        
+        <!-- The menu -->
+        <nav>
+            <ul>
+                <li><a href="./index.php">Home</a></li>
+                <li><a href="./index.php?m=profile">My Profile</a></li>
+            </ul>
+        </nav>
--- modules/partials/sidebar.php
+++ modules/partials/sidebar.php
@@ -1,11 +1,13 @@
+<?php
+    // Define an array to include all items in Sidebar
+    $features = ['Feature 1', 'Feature 2', 'Feature 3', 'Feature 4', 'Feature 5', 'Feature 6'];
+?>
 <div id="sidebar">
-				<h3> Sidebar </h3>
-				<ul>
-					<li>Feature 1</li>
-					<li>Feature 2</li>
-					<li>Feature 3</li>
-					<li>Feature 4</li>
-					<li>Feature 5</li>
-					<li>Feature 6</li>
-				</ul>
-			</div>
+    <h3> Sidebar </h3>
+    <ul>
+    <!-- Loop though the array and ouput to HTML -->
+    <?php foreach($features as $feature) { ?>
+        <li><?php echo $feature; ?></li>
+    <?php } ?>
+    </ul>
+</div>
